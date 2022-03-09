90f05ec1b6ebe5434a5b7a60ac5a05f4183666e4
Kousik Roy <kaushikroy1001@gmail.com>
2022-03-09 22:45:00 +0530

    second commit

--- figma/src/BreadCrumb.js
+++ figma/src/BreadCrumb.js
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaFilter } from "react-icons/fa";
+function BreadCrumb() {
+    const [show, setshow] = useState(true);
 
-function BreadCrumb(params) {
+    function showset() {
+        setshow(!show);
+    }
+
     return (<div>
-        <nav aria-label="breadcrumb">
-            <span class="breadcrumb">
-                <li class="breadcrumb-item" aria-current="page">Newest Ride</li>
-                <li class="breadcrumb-item" aria-current="page">Upcoming Ride</li>
-                <li class="breadcrumb-item" aria-current="page">Past Ride</li>
-            </span>
+        <nav className="breadcrum">
+            {/* <Link className="link-item" to="/nearest-ride"> */}
+            <li className="link-item">Newest Ride</li>
+            {/* </Link> */}
+            {/* <Link className="link-item" to="/past-ride"> */} <li className="link-item">Upcoming Ride</li>
+            {/* </Link> */}
+            {/* <Link className="link-item" to="/fut-ride"> */}
+            <li className="link-item">Past Ride</li>
+            {/* </Link> */}
+
+
+            <div className='filter'>
+
+                <button onClick={showset}>Filters  <FaFilter style={{ margin: "5px" }} /></button>
+
+                <div className='modals' style={{ display: show ? "none" : "" }}>
+
+                    <div className='title'>
+                        <h3>Filter</h3>
+                    </div>
+                    <select>
+                        <option>City</option>
+                    </select>
+                    <select>
+                        <option>State</option>
+                    </select>
+                </div>
+            </div>
         </nav>
-    </div>)
+    </div >)
 }
 
 export default BreadCrumb;
--- figma/src/Card.js
+++ figma/src/Card.js
@@ -2,25 +2,24 @@ import React from "react";
 
 function Card(prop) {
 
-
-    const props = prop.datas;
     return (
-
-        <div>
+        <div >
             <div className="CardLayout">
-                <div className="mapArea"></div>
+                <div className="mapArea">
+                    <img className="img" src={prop.datas.map_url} alt="img"></img>
+                </div>
                 <div className="CardArea">
                     <div>
-                        <div>Ride id: {props.id}</div>
-                        <div>Origin Station: {props.origin_station_code}</div>
-                        <div>Station_path:{props.Station_path}</div>
-                        <div>Date: {props.date}</div>
+                        <div>Ride id: {prop.datas.id}</div>
+                        <div>Origin Station: {prop.datas.origin_station_code}</div>
+                        <div>Station_path:{prop.datas.Station_path}</div>
+                        <div>Date: {prop.datas.date}</div>
                         <div>Distance:</div>
                     </div>
                 </div>
                 <div className="CardArea2">
-                    <div>{props.city}</div>
-                    <div>{props.state}</div>
+                    <div className="individualButton">{prop.datas.city}</div>
+                    <div className="individualButton">{prop.datas.state}</div>
                 </div>
             </div>
         </div>
